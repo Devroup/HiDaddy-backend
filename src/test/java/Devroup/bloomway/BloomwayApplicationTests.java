ae2b5bebbfeb3768e6771af290ca391ec0d8451c
권용현 <dydgus9952@naver.com>
2025-05-20 16:40:02 +0900

    chore: initialize project structure and basic configuration

---
+++ src/test/java/Devroup/bloomway/BloomwayApplicationTests.java
@@ -0,0 +1,13 @@
+package Devroup.bloomway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
